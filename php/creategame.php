24d07e27b543968f72391bae41b8da475906f30f
pnchappy92 <pchapman2892@students.mc3.edu>
2014-02-22 16:42:19 -0500

    Logans a lazy bum who did not want to do 2 min of work

--- php/creategame.php
+++ php/creategame.php
@@ -1,6 +1,24 @@
 <?php
 	require_once("establish.php");
 	
+	$sql = "SELECT MAX(gameid), totalweek FROM gametbl WHERE userid = (?)";
+	$stmt = mysqli_prepare($conn,$sql);
+	mysqli_stmt_bind_param($stmt, 'i', $userid);
+	mysqli_stmt_bind_result($stmt, $gameid, $totalweek)
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_fetch_result($stmt);
+	mysqli_stmt_close($stmt);
+	
+	$sql = "SELECT Count(gameid) FROM gametbl WHERE gameid = (?)"
+	$stmt = mysqli_prepare($conn,$sql);
+	mysqli_stmt_bind_param($stmt, 'i', $gameid);
+	mysqli_stmt_bind_result($stmt, $daysPlayed)
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_fetch_result($stmt);
+	mysqli_stmt_close($stmt);
+	
+	
+	
 	if($userid){
 		$sql = "INSERT INTO `gametbl` (`userid`) VALUES (?)";
 		$stmt = mysqli_prepare($conn,$sql);
