6768070255c4e0cc1f18af3260fc5d812236bf65
Logan <logan.murphy@temple.edu>
2014-02-22 13:06:52 -0500

    Organized files.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<script src="http://code.createjs.com/createjs-2013.12.12.min.js" type="text/javascript"></script>
+		<script src="db.js" type="text/javascript"></script>
+		<script src="game.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<canvas></canvas>
+	</body>
+</html>
