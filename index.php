de5f33ef473329030baa85ce189d59d80204fcd3
Logan <logan.murphy@temple.edu>
2014-02-23 04:38:15 -0500

    organized data sets

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!--
 git add -A
-git commit -m "junk is now written to the database"
+git commit -m "organized data sets"
 git pull https://github.com/nagolyhprum/phillycodefest14.git
 git push https://github.com/nagolyhprum/phillycodefest14.git
 -->
--- sql/initdb.sql
+++ sql/initdb.sql
@@ -36,29 +36,28 @@ CREATE TABLE IF NOT EXISTS `foodtbl` (
   `foodid` int(11) DEFAULT NULL AUTO_INCREMENT PRIMARY KEY,
   `foodgroupid` int(11) NOT NULL,
   `foodname` varchar(32) NOT NULL,
-  `foodimage` VARCHAR(32) NOT NULL,
-  `foodcalories` int(5) NOT NULL
+  `foodimage` VARCHAR(32) NOT NULL
 );
 
 INSERT INTO `foodtbl` (`foodgroupid`, `foodname`, `foodimage`, `foodcalories`) VALUES
-	(1, "Grape", "fruit_a.png", 10),
-	(1, "Apple", "fruit_b.png", 20),
-	(1, "Pineapple", "fruit_c.png", 30),
-	(2, "Carrot", "vegetable_a.png", 10),
-	(2, "Lettuce", "vegetable_b.png", 20),
-	(2, "Tomato", "vegetable_c.png", 30),
-	(3, "Bread", "grain_a.png", 10),
-	(3, "Cereal", "grain_b.png", 20),
-	(3, "Oatmeal", "grain_c.png", 30),
-	(4, "Whole Milk", "dairy_a.png", 10),
-	(4, "Cheese", "dairy_a.png", 20),
-	(4, "Half Milk", "dairy_a.png", 30),
-	(5, "Chicken", "protein_a.png", 10),
-	(5, "Lamb", "protein_b.png", 20),
-	(5, "Steak", "protein_c.png", 30),
-	(6, "Candy", "junk_a.png", 10),
-	(6, "Oil", "junk_b.png", 20),
-	(6, "Fat", "junk_c.png", 30);
+	(1, "Grape", "fruit_a.png"),
+	(1, "Apple", "fruit_b.png"),
+	(1, "Pineapple", "fruit_c.png"),
+	(2, "Carrot", "vegetable_a.png"),
+	(2, "Lettuce", "vegetable_b.png"),
+	(2, "Tomato", "vegetable_c.png"),
+	(3, "Bread", "grain_a.png"),
+	(3, "Cereal", "grain_b.png"),
+	(3, "Oatmeal", "grain_c.png"),
+	(4, "Whole Milk", "dairy_a.png"),
+	(4, "Cheese", "dairy_a.png"),
+	(4, "Half Milk", "dairy_a.png"),
+	(5, "Chicken", "protein_a.png"),
+	(5, "Lamb", "protein_b.png"),
+	(5, "Steak", "protein_c.png"),
+	(6, "Candy", "junk_a.png"),
+	(6, "Oil", "junk_b.png"),
+	(6, "Fat", "junk_c.png");
 
 CREATE TABLE IF NOT EXISTS `gametbl` (
   `gameid` int(11) NOT NULL AUTO_INCREMENT,
